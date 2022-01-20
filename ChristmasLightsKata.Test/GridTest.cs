550bd307f897df623416bf3c2eb40758482e404b
Foad Alavi <foadalavi@live.com>
2022-01-20 11:27:52 +0100

    Let's start the project

---
+++ ChristmasLightsKata.Test/GridTest.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ChristmasLightsKata.Test
+{
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ ChristmasLightsKata/Grid.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ChristmasLightsKata
+{
+    public class Grid
+    {
+    }
+}
